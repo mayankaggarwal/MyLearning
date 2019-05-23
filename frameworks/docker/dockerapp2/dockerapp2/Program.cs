a3c781133f1a36625df6333002dafc6871838a30
mayank <mayankaggarwal1989@gmail.com>
2019-05-23 08:53:14 +0530

    createdwithouthttps

--- frameworks/docker/dockerapp2/dockerapp2/Program.cs
+++ frameworks/docker/dockerapp2/dockerapp2/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace DockerDemo
+namespace dockerapp2
 {
     public class Program
     {
